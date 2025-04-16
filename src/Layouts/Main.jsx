151baaa22444a6150798ff8fcefa4b33895f5615
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-04-16 22:39:58 +0600

    create responsive navbar and starter section

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Starter from "../Components/Starter/Starter";
+import Navbar from "../Components/Navbar/Navbar";
 
 const Main = () => {
   return (
     <>
+      <Starter></Starter>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </>
   );
